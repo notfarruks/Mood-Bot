a56e6aa682e7177ca583bebbf0205074941e884a
Farrukh Mammadov <121432922+notfarruks@users.noreply.github.com>
2025-11-13 03:10:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require("telegraf")
 
-const BOT_KEY = "8458027195:AAEXzxdXkc8EllG0sEp3U_o00Cg9B_49dpw"
+const BOT_KEY = ""
 
 const bot = new Telegraf(BOT_KEY);
 
@@ -15,5 +15,6 @@ bot.hears(/.*/, (ctx)=>{
     const result = sentiment.analyze(ctx.message.text)
     return ctx.reply(`Score : ${result.score}\n Comparative Score: ${result.comparative}`)
 })
+
 
 bot.launch()
